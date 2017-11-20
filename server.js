62d35671150fbc863a441497b397f946cf575865
coreyarch1234 <harrilal@mit.edu>
2017-11-20 10:55:14 -0800

    ...

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.post('/latex', (req, res) => {
     console.log('--------------------------------------');
     // problem.renderedLatex = JSON.stringify(renderedLatex)
     // console.log(JSON.stringify(renderedLatex));
-    problem.renderedLatex = renderedLatex
-    console.log(renderedLatex);
+    problem.renderedLatex = `<span class="katex">`
+    console.log(problem.renderedLatex);
 
     // var p = new Problem(problem)
     // p.save().then().catch()
